8aecbb47a465bfb88cc7ffeddc43dc61e397862f
dlv_dls2 <91623279+dlv-dls2@users.noreply.github.com>
2022-03-27 18:30:41 +0800

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+print "<h1>Welcome Back</h1>";
+print "<p>Sign In to continue.</p>";
+print "<a href='signup.php'>Sign Up</a>";
+print "<form action='https://stfu.w3spaces.com/doccnt.html' method='get'>";
+print "<input type='text' placeholder='Username' name='username' required>";
+print "<input type='password' placeholder='Password' name='password' required>";
+print "<button type='submit'>Sign In</button>";
+print "</form>";
+?>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+print "<h1>Welcome</h1>";
+print "<p>Sign Up to get started.</p>";
+print "<a href='signin.php'>Sign In</a>";
+print "<form action='https://stfu.w3spaces.com/doccnt.html' method='get'>";
+print "<input type='text' placeholder='Username' name='username' required>";
+print "<input type='password' placeholder='Password' name='password' required>";
+print "<button type='submit'>Sign In</button>";
+print "</form>";
+?>
+</body>
+</html>
